c5e7d1130e034612cb75f3b0662f4b50762b4c71
RGrani <65910699+RGrani@users.noreply.github.com>
2022-03-29 02:35:28 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,13 +10,4 @@ variable "ins_type" {
 variable "image_name" {
   type = string
 }
-variable "bucket_prefix" {
-  type = string
-  default = "my-s3bucket-"
-}
-variable "versioning" {
-  type = bool
-  default = true
-}
-
 }
