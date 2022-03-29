890b16d2b82ddf98e1906f36da8c33dc6be6321e
RGrani02 <ranigupta9868@gmail.com>
2022-03-29 09:24:31 +0530

    adding' '

--- provider.tf
+++ provider.tf
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-=======
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
->>>>>>> parent of 943322d... Update provider.tf
-
 #configuring the AWS Provider
 
   provider "aws" {
